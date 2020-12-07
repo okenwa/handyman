e4e2ca76572859bd9a43a3cbe2873251c4a0c908
Okechukwu <okechukwu@skylinefuturistic.com>
2020-12-07 01:11:15 +0100

    Added theme constants and static fields

---
+++ lib/routes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+import 'main.dart';
+
+// We use name route
+// All our routes will be available here
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+};
